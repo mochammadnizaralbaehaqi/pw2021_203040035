4af7929b81e9c390f413dbfc80e74e4e346b68b6
mochammadnizar <mochammadnizaralbaehaqi@example.com>
2021-02-17 18:22:52 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Mochammad Nizar Albaehaqi
+203040035
+https://github.com/mochammadnizaralbaehaqi/pw2021_203040035
+Pertemuan 3 - 17 Februari 2021
+Mempelajari struktur kendali pada php
+*/
+?>
+
+<?php
+//Pengkondisian / percabangan
+//if else
+//if else if else
+//ternary
+//switch
+
+$x = 20;
+if( $x < 20 ){
+    echo "benar";
+}
+else if ( $x == 20){
+    echo "bingo!";
+}
+else {
+    echo "salah";
+}
+?>
